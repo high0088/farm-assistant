8c2cd075158f528ffd68b1fed8e72d560f77c68a
high0088 <high0088@gmail.com>
2026-05-29 14:55:36 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='ai-farm-iphone-v1';const FILES=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
